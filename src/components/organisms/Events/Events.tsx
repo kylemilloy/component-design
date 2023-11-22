64d5e937a83f07aabb9760583612b4412f1ce073
Kyle Milloy <k@fanda.io>
2023-11-21 23:08:13 -0700

    inline styles

--- src/components/organisms/Events/Events.tsx
+++ src/components/organisms/Events/Events.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  BoxProps,
   Grid,
   GridItem,
   Heading,
@@ -9,7 +10,6 @@ import {
   Input,
   Text,
 } from '@chakra-ui/react'
-import styled from '@emotion/styled'
 import afterFrame from 'afterframe'
 import { FC, ReactElement, useState } from 'react'
 import benchmark from '../../../utils/benchmark'
@@ -22,57 +22,24 @@ type EventItemProps = {
   onFavorite: (id: number) => void
 }
 
-const MainHeading = styled(Heading)`
-  margin: 1rem 0 0;
-`
-
-const SubHeading = styled(Heading)`
-  display: flex;
-  margin: 0.5rem 0;
-  text-transform: uppercase;
-  color: var(--chakra-colors-gray-500);
-`
-
-const StyledText = styled(Text)`
-  margin: 0.25rem 0 0;
-  color: var(--chakra-colors-gray-500);
-`
-
-const FullWidthImage = styled(Image)`
-  width: 100%;
-`
-
-const ImageContainer = styled(Box)`
-  position: relative;
-`
-
-const FavoriteButton = styled(IconButton)`
-  position: absolute;
-  top: 1rem;
-  right: 1rem;
-  align-items: center;
-  justify-content: center;
-`
-
-const FavoriteIcon = styled(Icon)`
-  width: 1.5rem;
-  height: 1.5rem;
-  color: red;
-`
-
 const EventItem: FC<EventItemProps> = ({ event, isFavorited, onFavorite }) => {
   const { name, image, location, description_without_html: description } = event
 
   return (
     <GridItem>
-      <ImageContainer>
-        <FullWidthImage src={image} />
+      <Box position="relative">
+        <Image w="full" src={image} />
 
-        <FavoriteButton
+        <IconButton
+          top="4"
+          right="4"
+          position="absolute"
+          alignItems="center"
+          justifyContent="center"
           aria-label="Toggle Favorite"
           onClick={() => onFavorite(event.id)}
         >
-          <FavoriteIcon>
+          <Icon w="6" h="6" color="red">
             <path
               fill={isFavorited ? 'currentColor' : 'none'}
               stroke={'currentColor'}
@@ -80,19 +47,27 @@ const EventItem: FC<EventItemProps> = ({ event, isFavorited, onFavorite }) => {
               strokeLinejoin="round"
               d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
             />
-          </FavoriteIcon>
-        </FavoriteButton>
-      </ImageContainer>
+          </Icon>
+        </IconButton>
+      </Box>
 
-      <MainHeading size="md" noOfLines={1}>
+      <Heading mt="4" size="md" noOfLines={1}>
         {name}
-      </MainHeading>
+      </Heading>
 
-      <SubHeading size="xs">
+      <Heading
+        my="2"
+        size="xs"
+        display="flex"
+        color="gray.500"
+        textTransform="uppercase"
+      >
         {location.name}, {location.city}
-      </SubHeading>
+      </Heading>
 
-      <StyledText noOfLines={3}>{description?.substring(0, 140)}</StyledText>
+      <Text mt="1" color="gray.500" noOfLines={3}>
+        {description?.substring(0, 140)}
+      </Text>
     </GridItem>
   )
 }
@@ -100,28 +75,19 @@ const EventItem: FC<EventItemProps> = ({ event, isFavorited, onFavorite }) => {
 type EventListProps = {
   events: ReadonlyArray<Event>
   render: (event: Event) => ReactElement
-}
+} & BoxProps
 
-const EventGrid = styled(Grid)`
-  gap: 1rem;
-  margin-top: 1rem;
-`
-
-export const EventList: FC<EventListProps> = ({ events, render }) => {
+export const EventList: FC<EventListProps> = ({ events, render, ...props }) => {
   return (
-    <Box>
+    <Box {...props}>
       <Heading>Events</Heading>
 
-      <EventGrid templateColumns="repeat(2, 1fr)">
+      <Grid mt="4" gap="4" templateColumns="repeat(2, 1fr)">
         {events.map((event) => render(event))}
-      </EventGrid>
+      </Grid>
     </Box>
   )
 }
-
-const StyledEventList = styled(EventList)`
-  margin-top: 1rem;
-`
 
 const Events: FC = () => {
   const [query, setQuery] = useState('')
@@ -153,7 +119,8 @@ const Events: FC = () => {
         onChange={(e) => setQuery(e.target.value)}
       />
 
-      <StyledEventList
+      <EventList
+        mt="4"
         events={events}
         render={(event) => (
           <EventItem
