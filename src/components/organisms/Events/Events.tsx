4cb442b548dc4e0eec56c491472b0adbdf204153
Kyle Milloy <k@fanda.io>
2023-11-21 23:22:50 -0700

    events

--- src/components/organisms/Events/Events.tsx
+++ src/components/organisms/Events/Events.tsx
@@ -10,33 +10,10 @@ import {
   Input,
   Text,
 } from '@chakra-ui/react'
-import { ReactElement, useCallback, useMemo, useState } from 'react'
-import data from './data.json'
+import { ReactElement } from 'react'
 import { Event } from './types'
 
 export default function Events() {
-  const [query, setQuery] = useState('')
-  const [favorites, setFavorites] = useState<Array<Event['id']>>([])
-  const events = useMemo(
-    () =>
-      data.filter((event) => {
-        if (query) {
-          return event.name.toLowerCase().includes(query.toLowerCase())
-        }
-
-        return true
-      }) as unknown as ReadonlyArray<Event>,
-    [query],
-  )
-
-  const onToggleFavorite = useCallback((id: number) => {
-    setFavorites((favorites) => {
-      return favorites.includes(id)
-        ? favorites.filter((favorite) => favorite !== id)
-        : [...favorites, id]
-    })
-  }, [])
-
   return (
     <Box>
       <Input
